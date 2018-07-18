f2a293564740573d83199d1a98266c6a38625a7f
Udenewu Kingsley <kingzpacking@gmail.com>
2018-07-18 11:03:36 +0100

    Update changes on payment transaction

--- src/Voguepay.php
+++ src/Voguepay.php
@@ -329,12 +329,6 @@ class Voguepay
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
             curl_setopt($ch, CURLOPT_URL, $url);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            if($this->proxy)
-            {
-                curl_setopt($ch, CURLOPT_PROXY, $this->proxy);
-                //curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
-                curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-            }
             curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windowos NT 5.1; en-NG; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13 Vyren Media-VoguePay API Ver 1.0");
             if(curl_errno($ch)){ curl_error($ch)." - [Called In getPaymentDetails() CURL]"; }
             $output = curl_exec($ch);
@@ -343,7 +337,6 @@ class Voguepay
         if($this->connection_type =="fgc")
         {
             $output = file_get_contents($url);
-            if(!$output) {$output = "Failed To Get JSON Data - [Called In getPaymentDetails() FGC]"; }
         }
         return $output;
     }
